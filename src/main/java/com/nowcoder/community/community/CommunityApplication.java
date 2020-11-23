6ede679e3112e284679b8fa9378deff8c26a2b32
Xu <1017390046@qq.com>
2020-11-23 12:22:10 +0800

    2020-11-23-12:22

--- src/main/java/com/nowcoder/community/community/CommunityApplication.java
+++ src/main/java/com/nowcoder/community/community/CommunityApplication.java
@@ -2,8 +2,10 @@ package com.nowcoder.community.community;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication
+
 public class CommunityApplication {
 
 	public static void main(String[] args) {
