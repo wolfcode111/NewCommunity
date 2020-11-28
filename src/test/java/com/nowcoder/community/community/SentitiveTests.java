619ace3ec9e9e5dfa23ca9feb035619cb450174d
Xu <1017390046@qq.com>
2020-11-28 23:31:27 +0800

    过敏敏感词

---
+++ src/test/java/com/nowcoder/community/community/SentitiveTests.java
@@ -0,0 +1,26 @@
+package com.nowcoder.community.community;
+
+import com.nowcoder.community.community.util.SensitiveFilter;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@ContextConfiguration(classes = CommunityApplication.class)
+public class SentitiveTests {
+
+    @Autowired
+    private SensitiveFilter sensitiveFilter;
+
+    @Test
+    public void testSenstiveFileter(){
+        String text = "这里可以赌博，可以嫖娼，可以开票，哈哈哈哈! ";
+        text =sensitiveFilter.filter(text);
+        System.out.println(text);
+    }
+
+}
