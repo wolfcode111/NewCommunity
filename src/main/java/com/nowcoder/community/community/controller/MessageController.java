f081fe774ba483d985271fd0d36e22f07b698dd6
Xu <1017390046@qq.com>
2020-12-07 20:33:50 +0800

    私信列表

--- src/main/java/com/nowcoder/community/community/controller/MessageController.java
+++ src/main/java/com/nowcoder/community/community/controller/MessageController.java
@@ -9,6 +9,7 @@ import com.nowcoder.community.community.util.HostHolder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -61,4 +62,43 @@ public class MessageController {
 
         return "/site/letter";
     }
+
+    @RequestMapping(path = "/letter/detail/{conversationId}",method = RequestMethod.GET)
+    public String getLetterDetail(@PathVariable("conversationId") String conversationId,Page page,Model model){
+        //分页信息
+        page.setLimit(5);
+        page.setPath("/letter/detail/"+conversationId);
+        page.setRows(messageService.findLetterCount(conversationId));
+
+        //私信列表
+        List<Message> letterList = messageService.findLetters(conversationId,page.getOffset(),page.getLimit());
+        List<Map<String,Object>> letters = new ArrayList<>();
+        if(letterList != null){
+            for(Message message : letterList){
+                Map<String,Object> map = new HashMap<>();
+                map.put("letter",message);
+                map.put("fromUser",userService.findUserById(message.getFromId()));
+                letters.add(map);
+            }
+        }
+
+        model.addAttribute("letters",letters);
+
+        //查询私信的目标
+        model.addAttribute("target",getLetterTarget(conversationId));
+
+        return "/site/letter-detail";
+    }
+
+    private  User getLetterTarget(String conversationId){
+        String[] ids = conversationId.split("_");
+        int id0 = Integer.parseInt(ids[0]);
+        int id1 = Integer.parseInt(ids[1]);
+         if(hostHolder.getUser().getId()==id0){
+             return userService.findUserById(id1);
+         }else{
+             return userService.findUserById(id0);
+         }
+    }
+
 }
