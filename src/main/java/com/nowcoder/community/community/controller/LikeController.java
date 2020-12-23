87f8fbe8078115abe20253e7d85ccda448e271e3
Xu <1017390046@qq.com>
2020-12-23 22:33:59 +0800

    2020-12-23 显示系统通知

--- src/main/java/com/nowcoder/community/community/controller/LikeController.java
+++ src/main/java/com/nowcoder/community/community/controller/LikeController.java
@@ -60,7 +60,7 @@ public class LikeController  implements CommunityConstant {
 
         return CommunityUtil.getJSONString(0,null,map);
         //返回Json的数据，因为是要做异步刷新
-
+       //为了提交到github上做的选择
     }
 
 
